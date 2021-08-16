49130e42997374960e642f07ee4ae57b729dd530
Renan Vaz <renan.piresvz@gmail.com>
2021-08-16 01:20:16 -0300

    Receber input de usuario para desenhar na tela (#6)

--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
 import * as world from './world'
-import { draw } from './canvas'
+import * as canvas from './canvas'
+import * as sand from './elements/sand'
+import * as stone from './elements/stone'
 
 const loop = () => {
   world.update()
-  draw(world)
-  // setTimeout(loop, 100)
+  canvas.draw(world)
+  requestAnimationFrame(loop)
 }
 
-const canvas = document.querySelector('#canvas')
+const $canvas = document.querySelector('#canvas')
+
+let drawing = false
+
+$canvas.addEventListener('mousemove', (e) => {
+  if (drawing)
+    world.set(
+      Math.floor(e.x / canvas.cellSize),
+      Math.floor(e.y / canvas.cellSize),
+      sand.make(),
+    )
+})
+$canvas.addEventListener('mousedown', () => {
+  drawing = true
+})
+$canvas.addEventListener('mouseup', () => {
+  drawing = false
+})
 
 world.init()
 loop()
