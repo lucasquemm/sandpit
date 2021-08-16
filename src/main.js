cc2470377d6b41cb3f8589425f9f082c092f213e
lucasquemm <62895234+lucasquemm@users.noreply.github.com>
2021-08-16 20:47:35 -0300

    implementacao da ui de selecao do elemento a desenhar no canvas (#7)
    * implementacao da ui de selecao do elemento a desenhar no canvas
    
    * adicao das modificacao sugerida na funcao de desenhar elemento e correcao de erro quando sem elemento selecionado

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import * as world from './world'
 import * as canvas from './canvas'
 import * as sand from './elements/sand'
 import * as stone from './elements/stone'
+import * as air from './elements/air'
 
 const loop = () => {
   world.update()
@@ -18,7 +19,7 @@ $canvas.addEventListener('mousemove', (e) => {
     world.set(
       Math.floor(e.x / canvas.cellSize),
       Math.floor(e.y / canvas.cellSize),
-      sand.make(),
+      useElement(),
     )
 })
 $canvas.addEventListener('mousedown', () => {
@@ -27,6 +28,31 @@ $canvas.addEventListener('mousedown', () => {
 $canvas.addEventListener('mouseup', () => {
   drawing = false
 })
+
+let selectedElement = 'air'
+
+const stoneBtn = document.querySelector('#pedra-btn')
+const sandBtn = document.querySelector('#areia-btn')
+const airBtn = document.querySelector('#ar-btn')
+
+const elements = { sand, stone, air }
+const useElement = () => elements[selectedElement].make()
+
+stoneBtn.addEventListener('click', () => {
+  return (selectedElement = 'stone')
+})
+
+sandBtn.addEventListener('click', () => {
+  return (selectedElement = 'sand')
+})
+
+airBtn.addEventListener('click', () => {
+  return (selectedElement = 'air')
+})
+
+const getElement = (element) => {
+  return element.make()
+}
 
 world.init()
 loop()
