5db083d83fe2222f725a94dccfe6950788251ec5
Renan Vaz <renan.piresvz@gmail.com>
2021-08-20 23:45:50 -0300

    Diminuir quantidade de cores

--- src/element.js
+++ src/element.js
@@ -1,4 +1,4 @@
-const rand = (max, min = 0) => Math.random() * (max - min) + min
+import { randInt } from './random'
 
 const make = ({ type, color, ...meta }) => ({
   type,
@@ -9,7 +9,7 @@ const make = ({ type, color, ...meta }) => ({
 })
 
 const createColor = (color) =>
-  `hsl(${color[0]}deg ${color[1]}% ${rand(color[2], color[3]).toFixed(1)}%)`
+  `hsl(${color[0]}deg ${color[1]}% ${randInt(color[2], color[3])}%)`
 
 const updateColor = (cell) => {
   cell.color = createColor(cell.colorInput)
