c6a0ea810e3fb3ea594f9f94f025f283558d7052
Lucas Brasil <62895234+lucasquemm@users.noreply.github.com>
2021-08-20 21:46:04 -0300

    implementado a atgualizacao horizontal das celulas de areia. (#13)

--- src/elements/sand.js
+++ src/elements/sand.js
@@ -1,6 +1,7 @@
 import * as air from './air'
 import * as water from './water'
 import * as element from '../element'
+import { pickRand } from '../random'
 
 const NAME = 'SAND'
 
@@ -8,6 +9,7 @@ const make = () => element.make({ type: NAME, color: [46, 78, 75, 50] })
 
 const update = (x, y, world) => {
   const below = world.get(x, y + 1)
+  const direction = pickRand([1, -1])
 
   switch (below.type) {
     case air.NAME:
@@ -15,6 +17,11 @@ const update = (x, y, world) => {
       break
     case water.NAME:
       world.swap(x, y, 0, 1)
+      break
+    case NAME:
+      if (world.is(x + direction, y + 1, air.NAME)) {
+        world.move(x, y, direction, 1)
+      }
       break
   }
 }
