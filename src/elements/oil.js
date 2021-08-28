4d0731e99d0a2c38af12a46c450d27a6a704165c
Lucas Brasil <lucasbcmoraes@gmail.com>
2021-08-28 17:25:53 -0300

    oleo wip

--- src/elements/oil.js
+++ src/elements/oil.js
@@ -1,16 +1,16 @@
 import { EMPTY } from './empty'
 import * as element from '../element'
 import * as fire from './fire'
-import * as oil from './oil'
+import * as water from './water'
 import { chance, pickRand } from '../random'
 
-const NAME = 'WATER'
+const NAME = 'OIL'
 
 const make = () =>
   element.make({
     type: NAME,
     direction: pickRand([1, -1]),
-    color: [216, 65, 60, 50],
+    color: [25, 59, 41, 48],
   })
 
 const update = (sandpit, cell) => {
@@ -21,21 +21,22 @@ const update = (sandpit, cell) => {
     case EMPTY:
       sandpit.move(0, 1)
       break
-    case oil.NAME:
-      sandpit.swap(0, 1)
-      break
     case NAME:
       if (sandpit.is(cell.direction, 1, EMPTY)) {
         sandpit.move(cell.direction, 1)
       }
       break
+    case water.NAME:
+      if (sandpit.is(cell.direction, -1, water.NAME)) {
+        sandpit.swap(cell.direction, -1)
+      }
+      break
   }
 
-  if (
-    sandpit.is(cell.direction, 0, EMPTY) ||
-    sandpit.is(cell.direction, 0, fire.NAME)
-  ) {
+  if (sandpit.is(cell.direction, 0, EMPTY)) {
     sandpit.move(cell.direction, 0)
+  } else if (chance(0.5) && sandpit.is(cell.direction, 0, water.NAME)) {
+    sandpit.swap(cell.direction, 0)
   } else {
     cell.direction *= -1
   }
