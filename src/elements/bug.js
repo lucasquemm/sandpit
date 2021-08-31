b04e3c6bce3515165d3f1a0a1e21a1102124c61e
Renan Vaz <renan.piresvz@gmail.com>
2021-08-31 20:41:50 -0300

    Adicionar voadores e puloes

--- src/elements/bug.js
+++ src/elements/bug.js
@@ -7,14 +7,23 @@ const BASE_COLOR = [277, 59, 50, 20]
 
 const NAME = 'BUG'
 
-const make = () =>
-  element.make({
+const make = () => {
+  const [species, color] = pickRand([
+    ['climber', BASE_COLOR],
+    ['jumper', [277, 62, 66, 60]],
+    ['flyer', [277, 33, 36, 30]],
+  ])
+
+  return element.make({
     type: NAME,
-    species: pickRand(['climber', 'jumper', 'flyer']),
+    species,
+    flammable: true,
+    jumpDirection: 0,
     climbing: false,
     direction: pickRand([1, -1]),
-    color: BASE_COLOR,
+    color: color,
   })
+}
 
 const update = (sandpit, cell) => {
   switch (cell.species) {
@@ -22,7 +31,11 @@ const update = (sandpit, cell) => {
       updateClimber(sandpit, cell)
       break
     case 'jumper':
+      updateJumper(sandpit, cell)
+      break
     case 'flyer':
+      updateFlyer(sandpit, cell)
+      break
   }
 
   for (let [nx, ny] of sandpit.neighbors1) {
@@ -63,11 +76,70 @@ const updateClimber = (sandpit, cell) => {
   }
 }
 
-const updateJumper = (sandpit, cell) => {}
+const updateJumper = (sandpit, cell) => {
+  if (cell.jumping) {
+    if (sandpit.is(cell.jumpDirection, -1, EMPTY)) {
+      sandpit.move(cell.jumpDirection, -1)
+    } else {
+      cell.jumping = false
+      cell.jumpDirection = pickRand([1, -1])
+    }
 
-const updateFlyer = (sandpit, cell) => {}
+    if (chance(0.1)) {
+      cell.jumping = false
+    }
+  } else {
+    const below = sandpit.get(cell.jumpDirection, 1)
+    const direction = pickRand([1, -1])
 
-const updateFall = (sandpit, cell) => {
+    switch (below.type) {
+      case EMPTY:
+        sandpit.move(cell.jumpDirection, 1)
+        break
+      case NAME:
+        if (sandpit.is(direction, 1, EMPTY)) {
+          sandpit.move(direction, 1)
+        }
+        break
+      default:
+        cell.jumpDirection = 0
+
+        if (chance(0.05)) {
+          cell.jumping = true
+          cell.jumpDirection = pickRand([1, 0, -1])
+        }
+        break
+    }
+
+    if (chance(0.2)) {
+      if (sandpit.is(cell.direction, 0, EMPTY)) {
+        sandpit.move(cell.direction, 0)
+      } else {
+        cell.direction *= -1
+      }
+    }
+  }
+}
+
+const updateFlyer = (sandpit, cell) => {
+  if (!cell.flyingDirection || chance(0.05)) {
+    const x = pickRand([1, 0, -1])
+    const y = pickRand(x === 0 ? [1, -1] : [1, 0, -1])
+
+    cell.flyingDirection = [x, y]
+    return
+  }
+
+  const [fx, fy] = cell.flyingDirection
+
+  if (sandpit.is(fx, fy, EMPTY)) {
+    sandpit.move(fx, fy)
+  } else if (chance(0.005)) {
+    cell.flyingDirection[pickRand([0, 1])] *= -1
+  }
+}
+
+const updateFall = (sandpit) => {
   const below = sandpit.get(0, 1)
   const direction = pickRand([1, -1])
 
