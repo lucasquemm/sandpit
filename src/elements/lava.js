0a9ad58f9fb3cdb1cd492af73e994079fb31ab91
Renan Vaz <renan.piresvz@gmail.com>
2023-03-30 19:52:41 -0300

    Fix lava interactions

--- src/elements/lava.js
+++ src/elements/lava.js
@@ -1,9 +1,12 @@
 import { EMPTY } from './empty'
+import * as smoke from './smoke'
+import * as stone from './stone'
 import * as element from '../element'
 import { chance, pickRand } from '../random'
 import * as fire from './fire'
 import { liquid } from '../traits/liquid'
 import burn from '../traits/burn'
+import { WATER } from './water'
 
 const color = 0xd67443
 const LAVA = 'LAVA'
@@ -12,15 +15,24 @@ const make = () =>
   element.make({
     type: LAVA,
     direction: pickRand([1, -1]),
+    liquid: true,
     color,
   })
 
 const update = (sandpit, cell) => {
   liquid(sandpit, cell, LAVA, { speed: 0.3 })
-  burn(sandpit)
+  const { fuel, igniteTarget } = burn(sandpit)
+
+  if (sandpit.is(pickRand([1, -1]), pickRand([0, 1, -1]), WATER)) {
+    sandpit.set(0, 0, stone.make())
+  }
 
   if (chance(0.005) && sandpit.is(0, -1, EMPTY)) {
     sandpit.set(0, -1, fire.make())
+  }
+
+  if (fuel && !igniteTarget && chance(sandpit.get(...fuel).flammability / 10)) {
+    sandpit.set(...fuel, smoke.make())
   }
 
   if (chance(0.005)) {
