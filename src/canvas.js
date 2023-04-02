47389c6c268f74585e3655ff38d6f9419b17056c
Renan Vaz <renan.piresvz@gmail.com>
2023-04-02 14:26:52 -0300

    Mobile support (#45)
    * Make canvas size dynamic
    
    * Support touch events
    
    * Adapt menu
    
    * Decrease cell size
    
    * Add favicon

--- src/canvas.js
+++ src/canvas.js
@@ -1,10 +1,14 @@
 import * as PIXI from 'pixi.js'
 
-const width = 600
-const height = 600
-const cellSize = 5
+const width = Math.min(600, window.innerWidth)
+const height = Math.max(600, window.innerHeight)
+const size = Math.min(width, height) - 20
+const cellSize = 3
+const cellsLength = Math.floor(size / cellSize)
+const totalCells = cellsLength * cellsLength
 
-const sprites = Array.from({ length: width / cellSize }, () => [])
+const sprites = Array.from({ length: cellsLength }, () => [])
+
 let texture
 let stage
 let renderer
@@ -14,35 +18,31 @@ const init = () => {
   const canvas = document.createElement('canvas')
 
   renderer = new PIXI.autoDetectRenderer({
-    width,
-    height,
+    width: size,
+    height: size,
     view: canvas,
   })
   document.querySelector('#canvas-target').prepend(canvas)
 
-  canvas.style.width = `${width}px`
-  canvas.style.height = `${height}px`
+  canvas.style.width = `${size}px`
+  canvas.style.height = `${size}px`
 
   stage = new PIXI.Container()
-  let total = 14400
 
   const graphic = new PIXI.Graphics()
   graphic.beginFill(0xffffff)
   graphic.drawRect(0, 0, 1, 1)
 
-  container = new PIXI.ParticleContainer(total, {
+  container = new PIXI.ParticleContainer(totalCells, {
     alpha: true,
   })
   texture = renderer.generateTexture(graphic)
 
-  container.scale.set(5, 5)
+  container.scale.set(cellSize, cellSize)
   stage.addChild(container)
 
-  const w = width / cellSize
-  const h = height / cellSize
-
-  for (let x = 0; x < w; x++) {
-    for (let y = 0; y < h; y++) {
+  for (let x = 0; x < cellsLength; x++) {
+    for (let y = 0; y < cellsLength; y++) {
       const sprite = new PIXI.Sprite(texture)
       sprite.position.x = x
       sprite.position.y = y
@@ -52,7 +52,7 @@ const init = () => {
     }
   }
 
-  return renderer.view
+  return { cellsLength, canvas: renderer.view }
 }
 
 const draw = (world) => {
